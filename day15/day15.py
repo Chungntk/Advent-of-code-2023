11d4c4e34b842b1aa192e4a5a2c2525ad14652c7
chungpro <chung.ngo@onemount.com>
2024-01-02 11:47:15 +0700

    day15 in progress

--- day15/day15.py
+++ day15/day15.py
@@ -8,47 +8,51 @@ def hash(chars):
     cur_value = 0
     for char in chars:
         cur_value = ((cur_value + ord(char)) * 17 ) % 256
-    return cur_value
+    return cur_value - 1
+
+def is_exist(input_label, row):
+    for id, (label, _) in enumerate(row):
+        if input_label == label:
+            return id
+    return -1
+    
+
+
 
 
 def main():
     
     lines = read_file('day15.txt')
-    # sum_results_part_1 = 0
-    
-    # for line in lines:
-    #     cur_value = hash(line)
-    #     sum_results_part_1 = sum_results_part_1 + cur_value
-    
-    # print(line, sum_results_part_1) 
-            
-    boxs = {}
-    for i in range(255):
-        key = i  # You can customize the key format as needed
-        value =  i+2, 3  # You can customize the value format as needed
-        boxs[key] = value
-    
-    print(boxs)
+    buckets = [[] for _ in range(255)]     
 
     for line in lines:
-        print(line)
         if line.find('=') != -1:
-            label = line.split('=')[0]
+            input_label = line.split('=')[0]
             focal_length = line.split('=')[1]
-            box_order = hash(label)
-            if box_order in boxs.keys(): 
-                boxs[box_order] = label, focal_length
+            if focal_length == '':
+                focal_length = 0
+
+            box_order = hash(input_label)
+            idx = is_exist(input_label, buckets[box_order])
+            if idx  > -1:
+                buckets[box_order][idx] = input_label, focal_length
             else:
+                buckets[box_order].append((input_label, focal_length))
+
                 
         else:
-            label = line.split('-')[0]
+            input_label = line.split('-')[0]
             focal_length = line.split('-')[1]
-            box_order = hash(label)
-            boxs[box_order] = label, focal_length
-
-            box = hash(line.split('-')[0])
-
-    #     print(box)
+            if focal_length == '':
+                focal_length = 0
+            box_order = hash(input_label)
+            idx = is_exist(input_label, buckets[box_order])
+            # print(idx)
+            if idx  > -1:
+                del buckets[box_order][idx]
+            
+    
+    print(buckets)
 
 
 
